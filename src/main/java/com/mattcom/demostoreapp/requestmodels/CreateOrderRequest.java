98236c38620caf9591b2ba99a6f89357768365a9
Matthew Tollner <matt.tollner@live.co.uk>
2024-04-08 13:19:02 +0200

    added missing file

---
+++ src/main/java/com/mattcom/demostoreapp/requestmodels/CreateOrderRequest.java
@@ -0,0 +1,18 @@
+package com.mattcom.demostoreapp.requestmodels;
+
+public class CreateOrderRequest {
+
+    int addressId;
+
+
+
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
+}
